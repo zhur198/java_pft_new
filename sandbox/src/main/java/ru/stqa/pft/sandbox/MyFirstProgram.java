4b385a6ba13ab04fe8d61ada3c87a8b3285ae80a
Zhur198 <zhur198@icloud.com>
2016-12-23 22:36:39 -0500

    added few new methods

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main (String[] args) {
+        hello("world");
+        hello("user");
+        hello("Inna");
 
-        System.out.println("Hello, world!");
+        double l = 5;
+        System.out.println("Squere with side " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Squere with sides " + a + " and " + b + " = " + area(a, b));
+    }
+
+    public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area (double len) {
+        return len * len;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 
 }

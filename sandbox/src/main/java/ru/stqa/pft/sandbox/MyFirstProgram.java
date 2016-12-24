95c4f9a7aaaccf13b80931fea98229c647cda64b
Zhur198 <zhur198@icloud.com>
2016-12-23 20:52:09 -0500

    changed settings

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,9 +2,9 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
+    public static void main (String[] args) {
 
-System.out.println ("Hello, world!"); 
-	}
+        System.out.println("Hello, world!");
+    }
 
 }

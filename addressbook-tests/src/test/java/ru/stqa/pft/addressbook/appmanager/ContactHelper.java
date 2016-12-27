2fbb765dadbad59238466867747cf745ca43dbd9
Zhur198 <zhur198@icloud.com>
2016-12-27 15:44:17 -0500

    added ContactModification Test

--- addressbook-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -28,4 +28,16 @@ public class ContactHelper extends HelperBase {
   public void openNewContactPage() {
       click(By.linkText("add new"));
   }
+
+  public void selectContact() {
+    click(By.name("selected[]"));
+  }
+
+  public void editContact() {
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void submitContactModification() {
+    click(By.name("update"));
+  }
 }
---
+++ addressbook-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModification.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+/**
+ * Created by zhur198 on 12/27/16.
+ */
+public class ContactModification extends TestBase{
+
+  @Test
+
+  public void testContactModification () {
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().editContact();
+    app.getContactHelper().fillNewContactForm(new ContactData("Ivan", "M", "Ivaniv", "407-499-0809", "ivaniv@somemail.com"));
+    app.getContactHelper().submitContactModification();
+    app.getNavigationHelper().gotoHomePage();
+  }
+}

e923b0edc34dc5aaa7f0c46375381d4f7fe1463f
Zhur198 <zhur198@icloud.com>
2016-12-27 15:25:25 -0500

    Added new test GroupModification

---
+++ addressbook-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by zhur198 on 12/27/16.
+ */
+public class GroupModificationTests extends TestBase{
+
+  @Test
+
+  public void testGroupModification () {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}

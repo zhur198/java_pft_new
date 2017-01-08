2e877f454e34a3378439f68d1ac354e091f5b433
Zhur198 <zhur198@icloud.com>
2017-01-08 11:47:34 -0500

    Replace == with Equals

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by zhur198 on 1/8/17.
+ */
+public class Equality {
+
+  public static void main (String [] args) {
+    String s1 = "firefox";
+    String s2 = new String(s1);
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+  }
+}

8436b8397c9639a5091aed2c58133e013768bcad
Zhur198 <zhur198@icloud.com>
2017-01-22 13:03:30 -0500

    Added For and While methods

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
@@ -0,0 +1,58 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by zhur198 on 1/22/17.
+ */
+public class Primes {
+
+  public static boolean isPrime (int n) {
+
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeFast (int n) {
+    int m = (int) Math.sqrt(n);
+    for (int i = 2; i < m; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeWhile1 (int n) {
+
+    int i = 2;
+    while (i < n) {
+      if (n % i == 0) {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+
+  public static boolean isPrimeWhile (int n) {
+
+    int i = 2;
+    while (i < n && n % i != 0) {
+      i++;
+    }
+    return i == n;
+  }
+
+  public static boolean isPrime (long n) {
+
+    for (long i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}

b408f37ba76ec86591d538afbb10a118e39037db
Zhur198 <zhur198@icloud.com>
2017-01-25 22:16:29 -0500

    delete last group insead of first

--- addressbook-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -36,8 +36,9 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup() {
-    click(By.name("selected[]"));
+  public void selectGroup(int index) {
+    wd.findElements(By.name("selected[]")).get(index).click();
+//    click(By.name("selected[]"));
   }
 
   public void initGroupModification() {

bbef22d1aff195886db575058ebd03b125cc3107
Zhur198 <zhur198@icloud.com>
2017-02-28 21:18:09 -0500

    Added logger to log errors, messages, info for GroupCreation

--- addressbook-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,16 +1,25 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.omg.CORBA.Object;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 /**
  * Created by zhur198 on 12/26/16.
  */
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
@@ -30,8 +39,18 @@ public class TestBase {
 //    app.stop();
 //  }
 //
-  @AfterSuite
+  @AfterSuite (alwaysRun = true)
   public void tearDown() {
   app.stop();
 }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parametrs " + Arrays.asList(p));
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
+  }
 }

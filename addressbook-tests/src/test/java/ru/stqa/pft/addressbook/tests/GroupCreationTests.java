92378a960989fbd5aab149b5b06e83ef963dafa1
Zhur198 <zhur198@icloud.com>
2017-03-02 18:40:43 -0500

    Added DB verification to Group Create and Delete Tests

--- addressbook-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -53,13 +53,24 @@ public class GroupCreationTests extends TestBase {
    }
   }
 
+//  @Test (dataProvider = "validGroupsFromJSON")
+//  public void testGroupCreation(GroupData group) {
+//    app.goTo().groupPage();
+//    Groups before = app.group().all();
+//    app.group().create(group);
+//    assertThat(app.group().count(), equalTo(before.size() + 1));
+//    Groups after = app.group().all();
+//    assertThat(after, equalTo(
+//            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+//  }
+
   @Test (dataProvider = "validGroupsFromJSON")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
@@ -68,11 +79,11 @@ public class GroupCreationTests extends TestBase {
   public void testBadGroupCreation() {
 
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData group = new GroupData().withName("test2'");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before));
   }
 
--- addressbook-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -11,21 +11,40 @@ import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
 
+//    @BeforeMethod
+//    public void ensurePreconditions () {
+//        app.goTo().groupPage();
+//        if (app.group().all().size() == 0) {
+//            app.group().create(new GroupData().withName("test1"));
+//        }
+//    }
+//
+//    @Test
+//    public void testGroupDeletion() {
+//        Groups before = app.group().all();
+//        GroupData deletedGroup = before.iterator().next();
+//        app.group().delete(deletedGroup);
+//        assertThat(app.group().count(), equalTo(before.size()-1));
+//        Groups after = app.group().all();
+//        assertThat(after, equalTo(before.without(deletedGroup)));
+//    }
+
     @BeforeMethod
     public void ensurePreconditions () {
-        app.goTo().groupPage();
-        if (app.group().all().size() == 0) {
+        if (app.db().groups().size() == 0){
+            app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1"));
         }
     }
-    
+
     @Test
     public void testGroupDeletion() {
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData deletedGroup = before.iterator().next();
+        app.goTo().groupPage();
         app.group().delete(deletedGroup);
         assertThat(app.group().count(), equalTo(before.size()-1));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.without(deletedGroup)));
     }
 }
